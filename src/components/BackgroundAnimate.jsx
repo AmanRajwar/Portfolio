33ac1e8409d2fd1d639c85ebd69cfb116810087c
AmanRajwar <amanrajwar79@gmail.com>
2025-02-17 00:25:40 +0530

    animated background done

--- src/components/BackgroundAnimate.jsx
+++ src/components/BackgroundAnimate.jsx
@@ -1,6 +1,9 @@
 import { useGSAP } from '@gsap/react'
 import React, { useRef, useState } from 'react'
 import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+gsap.registerPlugin(ScrollTrigger)
+
 
 const BackgroundAnimate = () => {
     const [state, setState] = useState(1)
@@ -11,31 +14,51 @@ const BackgroundAnimate = () => {
     useGSAP(() => {
         gsap.to(box1.current,
             {
-                x: 100,
+                x: 200,
                 duration: 1.2,
-                ease: "power2.out",
+                // ease: "power2.inOut",
                 scrollTrigger: {
                     trigger: box1.current,
-                    scroller: 'body', 
-                    start: "top top", 
-                    end: "bottom 50%+=100px",
-                    markers:true,
-                    scrub: 1,         
+                    start: "center-=135",
+                    end: "center+=200 end",
+                    scrub: 1,
+                }
+            })
+        gsap.to(box2.current,
+            {
+                x: -300,
+                duration: 1.2,
+                scrollTrigger: {
+                    trigger: box2.current,
+                    start: "center-=393vh",
+                    end: "center+=200 top",
+                    scrub: 1,
+                }
+            })
+        gsap.to(box3.current,
+            {
+                x: 400,
+                duration: 1.2,
+                scrollTrigger: {
+                    trigger: box3.current,
+                    start: "top-=",
+                    end: "center+=135 top",
+                    scrub: 1,
                 }
             })
     }, [])
     return (
-        <div className=" abc absolute text-color1 inset-0 flex flex-col justify-around overflow-hidden pointer-events-none  select-none  opacity-[0.07]">
+        <div className=" -rotate-12 absolute text-color1 inset-0 flex flex-col justify-around overflow-hidden items-center pointer-events-none  select-none  opacity-[0.07] -left-10">
 
-            <div className="relative whitespace-nowrap overflow-hidden py-2 border">
+            <div className="relative whitespace-nowrap overflow-hidden py-2 ">
                 <div ref={box1} class="inline-block  text-[8vw] font-display leading-none" >Creative Developer • Digital Artist • Interactive Designer • Creative Developer • Digital Artist • Interactive Designer • Creative Developer • Digital Artist • Interactive Designer • </div>
             </div>
-            {/* <div class="relative whitespace-nowrap overflow-hidden py-2">
+            <div ref={box2} class="relative whitespace-nowrap overflow-hidden py-2">
                 <div class="inline-block  text-[8vw] font-display leading-none">WebGL • Three.js • React • TypeScript • WebGL • Three.js • React • TypeScript • WebGL • Three.js • React • TypeScript • </div>
             </div>
-            <div class="relative whitespace-nowrap overflow-hidden py-2">
+            <div ref={box3} class="relative whitespace-nowrap overflow-hidden py-2">
                 <div class="inline-block  text-[8vw] font-display leading-none">Motion Design • 3D Animation • UI/UX • Motion Design • 3D Animation • UI/UX • Motion Design • 3D Animation • UI/UX • </div>
-            </div> */}
+            </div>
         </div>
     )
 }
