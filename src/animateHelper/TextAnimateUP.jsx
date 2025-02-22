3590ac4cebfa4441fea99a2b45251cfc013051bf
AmanRajwar <amanrajwar79@gmail.com>
2025-02-22 23:25:59 +0530

    created the text component to animate the text saperately

---
+++ src/animateHelper/TextAnimateUP.jsx
@@ -0,0 +1,23 @@
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+import gsap from "gsap";
+
+const TextAnimateUP = ({ str,tl, containerRef }) => {
+    const spanRefs = useRef([]); // Array to store span refs
+
+    return (
+        <div className="inline-block">
+            {str.split("").map((s, index) => (
+                <div
+                    key={`${index}-${s}`}
+                    // ref={(el) => (spanRefs.current[index] = el)} // Store ref for each span
+                    className="inline-block animateee"
+                >
+                    {s}
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default TextAnimateUP;
