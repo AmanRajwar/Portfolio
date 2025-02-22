424d61cd372433ba42d65d0bfd84920de717704b
AmanRajwar <amanrajwar79@gmail.com>
2025-02-22 23:24:22 +0530

    correction in home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import NavBar from '../../components/NavBar'
 import BackgroundAnimate from '../../components/BackgroundAnimate'
 import WelcomeSection from '../../components/WelcomeSection'
@@ -6,20 +6,26 @@ import { HeroContext } from '../../contexts/HeroAnimateContext'
 import HeroSection from '../../components/HeroSection'
 
 const Home = () => {
-  const { heroAnimate } = useContext(HeroContext)
-
+  const { isWelcomeAnimating } = useContext(HeroContext)
+  const [state, setState] = useState(true)
+  useEffect(() => {
+    if (isWelcomeAnimating)
+      setTimeout(() => {
+        setState(false)
+      }, 400)
+  }, [isWelcomeAnimating])
   return (
     <div className='bg-background min-w-[100dvw] relative'>
       <NavBar />
       <main className='bg-background'>
         <section className='  w-full min-h-[100svh]  relative   flex flex-col items-center justify-center '>
           <BackgroundAnimate />
-          <WelcomeSection />
-          <HeroSection/>
+          {/* {state ? <WelcomeSection /> : <HeroSection />} */}
+          <HeroSection />
         </section>
       </main>
       {
-        heroAnimate && <div className='min-h-[100vh]'>
+        isWelcomeAnimating && <div className='min-h-[100vh]'>
 
         </div>
       }
